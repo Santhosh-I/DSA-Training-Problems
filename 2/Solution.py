60c97c6ac8078981c3ba1a0dd1294ff97d5ccf08
Santhosh I <santhoshilaiyaraja2006@gmail.com>
2026-09-07 16:16:10 +0530

    Build in method

--- 2/Solution.py
+++ 2/Solution.py
@@ -1,5 +1,5 @@
 array = list(map(int,input("Enter array:").split()))
 
-print("First Largest :",max(array))
+print("First Largest :",max(array),"Index :",array.index(max(array))+1)
 array.remove(max(array))
-print("Second Largest :",max(array))
+print("Second Largest :",max(array),"Index :",array.index(max(array))+1)
