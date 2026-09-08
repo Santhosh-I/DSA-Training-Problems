85fa1e0e6c598a79892f582f8c3b13295c73b2f5
Santhosh I <santhoshilaiyaraja2006@gmail.com>
2026-09-08 19:28:10 +0530

    updated

--- 4/Solution.py
+++ 4/Solution.py
@@ -9,3 +9,22 @@ if count == 0:
     print("Yes")
 else:
     print("No")
+
+#With set fuction
+no_dup = set(array)
+
+if len(array) == len(no_dup):
+    print("Yes")
+else:
+    print("No")
+
+#
+res = "Yes"
+dup = []
+for i in array:
+    if i not in dup:
+        dup.append(i)
+    else:
+        res = "No"
+
+print(res)
