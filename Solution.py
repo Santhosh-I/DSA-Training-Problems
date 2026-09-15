32d17561695703730837e0c881868b7c65ce5586
Santhosh I <santhoshilaiyaraja2006@gmail.com>
2026-09-15 23:16:00 +0530

    Folder segregated

--- Solution.py
+++ Solution.py
@@ -1,6 +1,13 @@
-s = [1,2,3]
+nums = [4,3,2,7,8,2,3,1]
 
-arr = "".join(map(str,s))
-a = int(arr)
-res = a + 1
-print(list(str(res)))
+freq = {}
+res = []
+
+for i in nums:
+    freq[i] = freq.get(i,0) + 1
+
+for i in nums:
+    if freq[i] > 1 and i not in res:
+        res.append(i)
+
+print(res)
